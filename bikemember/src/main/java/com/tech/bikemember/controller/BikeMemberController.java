fffd51f3d3565f6f267b728538fc26159ca0b0df
li_yeon <fldus612@gmail.com>
2023-10-25 16:50:45 +0900

    🍃DAY 125 CRUD - update,delete - @GetMapping, PutMapping, DeleteMapping - ResponseEntity - PathVariable

--- bikemember/src/main/java/com/tech/bikemember/controller/BikeMemberController.java
+++ bikemember/src/main/java/com/tech/bikemember/controller/BikeMemberController.java
@@ -1,8 +1,11 @@
 package com.tech.bikemember.controller;
 
+import com.tech.bikemember.exception.ResourceNotFoundException;
 import com.tech.bikemember.model.BikeMember;
 import com.tech.bikemember.repository.BikeMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -28,5 +31,42 @@ public class BikeMemberController {
     public BikeMember createBikeMember(@RequestBody BikeMember bikeMember){
         System.out.println("BikeMember createBikeMemberad");
         return bikeMemberRepository.save(bikeMember);
+    }
+
+//    @PostMapping("/num")
+//    public void createBikeMemberNumber(@RequestBody BikeMember bikeMember){
+//        System.out.println("void createBikeMemberNumber 신호가 들어오는지만 확인");
+//    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<BikeMember> getBikeMemberById(@PathVariable long id){
+        System.out.println("ResponseEntity<BikeMember> getBikeMemberById : "+id);
+        BikeMember bikeMember=bikeMemberRepository.findById(id).
+                orElseThrow(()->new ResourceNotFoundException("BikeMember Not Exist with id : "+id));
+        return ResponseEntity.ok(bikeMember);
+    }
+    @PutMapping("{id}")
+    public ResponseEntity<BikeMember> updateBikeMemBer(@PathVariable long id,
+                                                       @RequestBody BikeMember bikeMember){
+        System.out.println("ResponseEntity<BikeMember> updateBikeMemBer : "+id);
+        BikeMember updateBikeMember=bikeMemberRepository.findById(id).
+                orElseThrow(()->new ResourceNotFoundException("BikeMember Not Exist with id : "+id));
+        updateBikeMember.setFirstName(bikeMember.getFirstName());
+        updateBikeMember.setLastName(bikeMember.getLastName());
+        updateBikeMember.setEmailId(bikeMember.getEmailId());
+
+        bikeMemberRepository.save(updateBikeMember);
+        return ResponseEntity.ok(updateBikeMember);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<BikeMember> deleteBikeMember(@PathVariable long id){
+        System.out.println("ResponseEntity<BikeMember> deleteBikeMember : "+id);
+        System.out.println(id+"번 글을 삭제합니다.");
+        BikeMember bikeMember=bikeMemberRepository.findById(id).
+                orElseThrow(()->new ResourceNotFoundException("BikeMember Not Exist with id : "+id));
+        bikeMemberRepository.delete(bikeMember);
+
+        return new ResponseEntity <> (HttpStatus.NO_CONTENT);
     }
 }
